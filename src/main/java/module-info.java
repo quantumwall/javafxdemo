a96d873a217cb9dacc945d72f080f6955bd5a254
a.zhurkin <a.zhurkin@infomaximum.com>
2024-10-24 17:47:00 +0300

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module org.quantum.javafxdemo {
+    requires javafx.controls;
+    exports org.quantum.javafxdemo;
+}
---
+++ src/main/java/org/quantum/javafxdemo/App.java
@@ -0,0 +1,17 @@
+package org.quantum.javafxdemo;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) {
+	stage.show();
+    }
+
+    public static void main(String[] args) {
+	launch();
+    }
+
+}
